78812307491af6a60203bbb0bdb6604dc9343b2d
xiaomiao-fong <tomylin567@gmail.com>
2025-03-05 08:07:11 +0000

    rename and remove unused variable

--- src/mediator/mediator/mediator.py
+++ src/mediator/mediator/mediator.py
@@ -14,31 +14,23 @@ from mediator.constants import NUM_DRONES, NUM_BUTTONS
 class Mediator(Node):
 
     def __init__(self, path: str):
-        with open(path, "r") as f:
-            config = yaml.load(f)
 
         super.__init__("Mediator")
         self.get_logger().info("init mediator")
 
-        self.MainDrone = Drone(config["MainDronePrefix"], 1, self)
+        self.MainDrone = Drone(1, self)
         self.SubDrones = []
-        for subdrone in config["SubDronePrefixes"]:
-            self.SubDrones.append(Drone(subdrone, 1, self)) # should the id be 2 and 3???
+        for i in range(2,4):
+            self.SubDrones.append(Drone(i, self)) # should the id be 2 and 3???
 
         self.controlled_drone: Drone = self.MainDrone
-        self.execute()
 
-        self.create_timer(0.03, self.execute)
 
         self.prev_buttons = [False] * NUM_BUTTONS
-        self.__teleop_control = False
-        self.__magnet_control = False
-        self.__drop_control = False
-        self.__drone_control = 0
-        self.__vx = 0.0
-        self.__vy = 0.0
-        self.__vz = 0.0
-        self.__yaw = 0.0
+        self.__teleop_btn_signal = False
+        self.__magnet_btn_signal = False
+        self.__drop_btn_signal = False
+        self.__current_drone = 1
 
         qos_profile = QoSProfile(
             reliability=QoSReliabilityPolicy.BEST_EFFORT,
@@ -54,26 +46,21 @@ class Mediator(Node):
         pass
 
     def __set_esp_values(self, msg):
-        self.__vx = msg.vx
-        self.__vy = msg.vy
-        self.__vz = msg.vz
-        self.__yaw = msg.yaw
         buttons = msg.buttons
 
-        if self.prev_buttons[0] ^ buttons[0]:
-            self.__teleop_control = True
-        if self.prev_buttons[1] ^ buttons[1]:
-            self.__magnet_control = True
-        if self.prev_buttons[2] ^ buttons[2]:
-            self.__drop_control = True
+        self.__teleop_btn_signal = self.prev_buttons[0] ^ buttons[0]
+        self.__magnet_btn_signal = self.prev_buttons[1] ^ buttons[1]
+        self.__drop_btn_signal = self.prev_buttons[2] ^ buttons[2]             
 
         for i in range(NUM_DRONES):
             if self.prev_buttons[3+i] ^ buttons[3+i]:
-                self.__drone_control = i
+                self.__current_drone = i + 1
                 # self.prev_buttons = [False] * NUM_BUTTONS
-                self.__teleop_control = False
-                self.__magnet_control = False
-                self.__drop_control = False
+                self.__teleop_btn_signal = False
+                self.__magnet_btn_signal = False
+                self.__drop_btn_signal = False
+
+        self.execute() # TODO Is there a better way
 
         self.prev_buttons = buttons
 
