eff1ec1740ff567b4088323910779cfbbd0c4f91
xiaomiao-fong <tomylin567@gmail.com>
2025-03-20 00:07:24 +0000

    final ver without servo

--- src/mediator/mediator/mediator.py
+++ src/mediator/mediator/mediator.py
@@ -18,7 +18,8 @@ class Mediator(Node):
         super().__init__("Mediator")
         self.get_logger().info("init mediator")
 
-        self.drones = [Drone(i, self) for i in range(4)]
+        self.ids = [1,7,8]
+        self.drones = [Drone(self.ids[i], self) for i in range(3)]
 
         self.prev_buttons = [False] * NUM_BUTTONS
         self.__teleop_btn_signal = False
@@ -28,6 +29,7 @@ class Mediator(Node):
         self.__next_drone = -1
 
         self.__first = True
+        self.__vel = [1500, 1500, 1500, 1500]
 
         qos_profile = QoSProfile(
             reliability=QoSReliabilityPolicy.BEST_EFFORT,
@@ -41,27 +43,25 @@ class Mediator(Node):
 
     def execute(self):
 
-        print(f"drone_1 : {' on' if self.__current_drone == 1 else 'off'}, drone_2 : {' on' if self.__current_drone == 2 else 'off'},\
-            drone_3 : {' on' if self.__current_drone == 3 else 'off'}")
+        print(f"drone_{self.ids[0]} : {' on' if self.__current_drone == 0 else 'off'}, drone_{self.ids[1]} : {' on' if self.__current_drone == 1 else 'off'}, drone_{self.ids[2]} : {' on' if self.__current_drone == 2 else 'off'}, vel: {self.__vel}")
 
         if self.__current_drone == -1 and self.__next_drone == -1 : return
 
-        if self.__current_drone != self.__next_drone:
+        if self.__current_drone != self.__next_drone and self.__current_drone != -1:
             current_drone = self.drones[self.__current_drone]
             current_drone.idle()
+            print(self.__current_drone, "idle")
             self.__current_drone = -1
         
         if self.__current_drone == -1 and not self.__next_drone == -1:
             current_drone = self.drones[self.__next_drone]
             current_drone.arm()
-            # print(self.__next_drone, "arm")
             self.__current_drone = self.__next_drone
             return
         
         if self.__current_drone == -1 : return
 
         current_drone = self.drones[self.__current_drone]
-        drone_state = current_drone.drone_state
 
         if self.__magnet_btn_signal:
             current_drone.load()
@@ -69,15 +69,32 @@ class Mediator(Node):
         if self.__drop_btn_signal:
             current_drone.drop()
 
-        if self.__teleop_btn_signal and drone_state == AgentStatus.STATE_ARM:
-            current_drone.teleop()
+        if current_drone.is_armed:
+            current_drone.teleop(self.__vel)
             return
             
+    def vel_mapping(self, vels):
+        # vels : [vx, vy, vz, yawRate]
+        # velocities : AETR
 
+        velocities = [1500, 1500, 1500, 1500]
+        velocities[0] += vels[1]*500
+        velocities[1] += vels[0]*500
+        velocities[2] += vels[2]*500
+        velocities[3] += vels[3]*500
+
+        return velocities
         
 
     def __set_esp_values(self, msg):
+        self.__vel = self.vel_mapping([msg.vx, msg.vy, msg.vz, msg.yaw])
         buttons = msg.buttons
+
+        if self.__first :
+            self.__first = False
+            self.prev_buttons = buttons
+            print(self.__current_drone)
+            return
 
         self.__next_drone = self.__current_drone
 
@@ -87,15 +104,14 @@ class Mediator(Node):
 
         for i in range(NUM_DRONES):
             if self.prev_buttons[3+i] ^ buttons[3+i]:
-                self.__next_drone = i+1 if self.__current_drone != i+1 else -1
+                self.__next_drone = i if self.__current_drone != i else -1
                 # self.prev_buttons = [False] * NUM_BUTTONS
                 self.__teleop_btn_signal = False
                 self.__magnet_btn_signal = False
                 self.__drop_btn_signal = False
 
         # print(self.__next_drone)
-        if not self.__first : self.execute() # TODO Is there a better way
-        self.__first = False
+        self.execute() # TODO Is there a better way
 
         self.prev_buttons = buttons
 
--- src/mediator/mediator/virtual_drone.py
+++ src/mediator/mediator/virtual_drone.py
@@ -6,6 +6,8 @@ from px4_msgs.msg import (GotoSetpoint, OffboardControlMode, TrajectorySetpoint,
 
 from esp_msg.msg import ESPCMD, AgentStatus
 from std_msgs.msg import Bool, UInt32
+from espkinesis_msgs.msg import ChannelOverride
+from safmc_msgs.msg import Magnet
 
 class Drone():
 
@@ -15,10 +17,17 @@ class Drone():
         self.mediator_node = node
         self.drone_prefix = f"/drone_{self.id}"
         self.px4_prefix = f"/px4_{self.id}"
+        self.target_prefix = f"/target_{self.id}"
+
+        self.drop_cnt = 0
 
         self.__arm_ready_signal: bool = False
         self.__status_signal: AgentStatus = None
         self.__loaded_signal: bool = False
+
+        self._is_armed = False
+        self._is_each_pre_flight_check_passed = False
+        self._vehicle_timestamp = 1
 
         qos_profile = QoSProfile(
             reliability=QoSReliabilityPolicy.BEST_EFFORT,
@@ -27,13 +36,27 @@ class Drone():
             depth=1
         )
 
+        qos_target = QoSProfile(
+            depth=10
+        )
+
+        # ESP Kinesis
+
+        self.__target_publisher = node.create_publisher(
+            ChannelOverride,
+            f'{self.target_prefix}/espk/channel_override',
+            qos_target
+        )
+
         # # Subscriber for px4
-        # self.vehicle_status_sub = node.create_subscription(
-        #     VehicleStatus,
-        #     f"{self.px4_prefix}/fmu/out/vehicle_status",
-        #     self.__set_vehicle_status,
-        #     qos_profile
-        # )
+
+        self.vehicle_status_sub = node.create_subscription(
+            VehicleStatus,
+            f"{self.px4_prefix}/fmu/out/vehicle_status",
+            self.__set_vehicle_status,
+            qos_profile
+        )
+
         # Subscriber for drone
         self.arm_ready_sub = node.create_subscription(
             UInt32,
@@ -60,7 +83,7 @@ class Drone():
         self.__idle_pub   = node.create_publisher(Bool, f"{self.px4_prefix}/in/idle", qos_profile)
         self.__arm_pub    = node.create_publisher(Bool, f"{self.px4_prefix}/in/arm", qos_profile)
         self.__teleop_pub = node.create_publisher(Bool, f"{self.px4_prefix}/in/teleop", qos_profile)
-        self.__load_pub   = node.create_publisher(Bool, f"{self.px4_prefix}/in/load", qos_profile)
+        self.__magnet_pub   = node.create_publisher(Magnet, f"{self.px4_prefix}/payload/in/magnet", 10)
         self.__hold_pub   = node.create_publisher(Bool, f"{self.px4_prefix}/in/hold", qos_profile)
         self.__drop_pub   = node.create_publisher(Bool, f"{self.px4_prefix}/in/drop", qos_profile)
         self.__track_pub  = node.create_publisher(Bool, f"{self.px4_prefix}/in/track", qos_profile)
@@ -82,8 +105,26 @@ class Drone():
     def drone_state(self) -> int:
         if self.__status_signal is None: return 8
         return int(self.__status_signal.state)
+    
+    @property
+    def is_armed(self) -> bool:
+        return self._is_armed
+    
+    @property
+    def is_pf_ok(self) -> bool:
+        return self._is_each_pre_flight_check_passed
 
     ### Setters ###
+
+    def __set_vehicle_status(self, vehicle_status_msg: VehicleStatus) -> None:
+        self._is_each_pre_flight_check_passed = (
+            vehicle_status_msg.pre_flight_checks_pass
+        )
+        self._vehicle_timestamp = vehicle_status_msg.timestamp
+        self._is_armed = (
+            vehicle_status_msg.arming_state == VehicleStatus.ARMING_STATE_ARMED
+        )
+
     def __set_arm_ready_signal(self, msg : UInt32) -> None:
         self.__arm_ready_signal = msg.data == self.id
     
@@ -96,24 +137,53 @@ class Drone():
     ### signal ###
 
     def arm(self):
-        self.__arm_pub.publish(Bool(**{"data" : True}))
 
-    def teleop(self):
-        self.__teleop_pub.publish(Bool(**{"data" : True}))
+        print(self.id, "arm")
+        msg = ChannelOverride()
+
+        msg.channels = [-1,-1,-1,-1,2000]
+        msg.duration = 1000
+
+        msg.bypass_safety = True
+        self.__target_publisher.publish(msg)
+
+    def teleop(self, velocities):
+        msg = ChannelOverride()
+
+        print(velocities)
+        msg.channels = [int(velocities[0]),int(velocities[1]),int(velocities[2]),int(velocities[3])]
+        msg.duration = 1000
+
+        self.__target_publisher.publish(msg)
 
     def load(self):
-        self.__load_pub.publish(Bool(**{"data" : True}))
+        msg = Magnet()
+        self.drop_cnt = 0
+        msg.magnet1 = True
+        msg.magnet2 = True
+        msg.magnet3 = True
+
+        self.__magnet_pub.publish(msg)
 
     def hold(self):
         self.__hold_pub.publish(Bool(**{"data" : True}))
 
     def drop(self):
-        self.__drop_pub.publish(Bool(**{"data" : True}))
+        msg = Magnet()
+        msg.magnet1 = self.drop_cnt < 0
+        msg.magnet2 = self.drop_cnt < 1
+        msg.magnet3 = self.drop_cnt < 2
+
+        self.drop_cnt += 1
+        self.drop_cnt %= 3
+
+        self.__magnet_pub.publish(msg)
 
     def track(self):
         self.__track_pub.publish(Bool(**{"data" : True}))
 
     def idle(self):
+        print(self.id, "idle")
         self.__idle_pub.publish(Bool(**{"data" : True}))
     
     # util function
